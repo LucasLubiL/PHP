4d3a2705b25467026f601a4008199d4584e3eb6c
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2025-03-12 22:14:25 -0300

    Add files via upload

---
+++ WEB 1/cadastro.php
@@ -0,0 +1,13 @@
+<?php
+
+   $nome = $_GET["nome"];
+   $tutor = $_GET["tutor"];
+   echo "Nome: $nome e Tutor: $tutor
+   <br/><br/>";
+   $con = mysqli_connect("localhost", "root", "vertrigo", "veterinaria");
+
+   $sql = "INSERT INTO animal (nome, tutor) values ('$nome','$tutor')";
+
+   @$resultado = mysqli_query($con, $sql);
+
+?>
