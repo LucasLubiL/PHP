0ba96d37c03f5ace93705b2728a004f033707c3a
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2025-03-27 20:35:47 -0300

    Add files via upload

--- WEB 1/agendas/vc/modelo/ContatoDAO.php
+++ WEB 1/agendas/vc/modelo/ContatoDAO.php
@@ -16,6 +16,26 @@
             $this->con = $conF->getConnection();
         }
 
+        public function cadastrar($cont){
+            try{
+                $stmt = $this->con->prepare(
+                   "INSERT INTO contato(nome, email, telefone)
+                   values (:nome, :email, :telefone)"
+                );
+                
+
+                $stmt->bindValue(":nome", $cont->getNome());
+                $stmt->bindValue(":email", $cont->getEmail());
+                $stmt->bindValue(":telefone", $cont->getTelefone());
+
+                $stmt->execute();
+
+            }
+            catch(PDOException $ex){
+                echo"Erro: " . $ex->getMessage();
+            }
+        }
+
     }
 
 ?>
